94a330a0588a6c6536b91ff3d00f92d681349881
youyinian288 <youyinian288@gmail.com>
2025-10-07 02:01:42 +0800

    完成示例项目重命名：将RichEditorViewSample完全重命名为AITextViewSample
    - 重命名剩余的RichEditorViewSample目录
    - 更新Xcode项目文件中的所有目标名称引用
    - 更新Podfile和Pods配置
    - 清理旧的构建文件和Pods目标文件
    - 确保所有示例项目文件使用新的命名规范

--- AITextViewSample/AITextViewUIKIT/Pods/Target Support Files/Pods-AITextViewSample/Pods-AITextViewSample-umbrella.h
+++ AITextViewSample/AITextViewUIKIT/Pods/Target Support Files/Pods-AITextViewSample/Pods-AITextViewSample-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_RichEditorViewSampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_RichEditorViewSampleVersionString[];
+FOUNDATION_EXPORT double Pods_AITextViewSampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AITextViewSampleVersionString[];
 
